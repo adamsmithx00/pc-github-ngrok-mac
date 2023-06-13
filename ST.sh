c2e4eea354e8d06d7643aef846820b546e299783
Jahid Hasan <130596733+jahidrdpschool@users.noreply.github.com>
2023-06-13 22:57:28 +0600

    Update ST.sh

--- ST.sh
+++ ST.sh
@@ -8,8 +8,8 @@ sudo dscl . -create /Users/TheDisala RealName The_Disala_YT
 sudo dscl . -create /Users/TheDisala UniqueID 1001
 sudo dscl . -create /Users/TheDisala PrimaryGroupID 80
 sudo dscl . -create /Users/TheDisala NFSHomeDirectory /Users/tcv
-sudo dscl . -passwd /Users/TheDisala @#Jahid123456
-sudo dscl . -passwd /Users/TheDisala @#Jahid123456
+sudo dscl . -passwd /Users/TheDisala @#Disala123456
+sudo dscl . -passwd /Users/TheDisala @#Disala123456
 sudo createhomedir -c -u TheDisala > /dev/null
 sudo dscl . -append /Groups/admin GroupMembership username
 
@@ -17,7 +17,7 @@ sudo dscl . -append /Groups/admin GroupMembership username
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
 
-echo @#Jahid123456 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+echo @#Disala123456 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 
 #Start VNC
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
