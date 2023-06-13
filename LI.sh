ec8be59314e48a1f6e28449a99fea67c0df0ae75
Jahid Hasan <130596733+jahidrdpschool@users.noreply.github.com>
2023-06-13 22:30:35 +0600

    Update LI.sh

--- LI.sh
+++ LI.sh
@@ -4,5 +4,5 @@ echo .....Note: Make suer to use Realvnc Viewer to connect.....
 echo ..........................................................
 echo IP:
 curl -s http://localhost:4040/api/tunnels | grep -o '"public_url":"[^"]*' | sed 's/"public_url":"//'
-echo Username: TheDisala
-echo Password: @#Disala123456
+echo Username: JahidHasan
+echo Password: @#Jahid123456
