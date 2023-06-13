abe247541a26faa33bad98882db7cebac2c68469
Jahid Hasan <130596733+jahidrdpschool@users.noreply.github.com>
2023-06-13 22:38:56 +0600

    Update ST.sh

--- ST.sh
+++ ST.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo mdutil -i off -a
 
-#Create new account
+#Create account
 sudo dscl . -create /Users/JahidHasan
 sudo dscl . -create /Users/JahidHasan UserShell /bin/bash
-sudo dscl . -create /Users/JahidHasan RealName The_Disala_YT
+sudo dscl . -create /Users/JahidHasan RealName Jahid_Hasan
 sudo dscl . -create /Users/JahidHasan UniqueID 1001
 sudo dscl . -create /Users/JahidHasan PrimaryGroupID 80
 sudo dscl . -create /Users/JahidHasan NFSHomeDirectory /Users/tcv
@@ -17,13 +17,13 @@ sudo dscl . -append /Groups/admin GroupMembership username
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
 
-echo @#Disala123456 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+echo @#Jahid123456 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 
-#Start VNC/reset changes
+#Start VNC
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
-#Start & Down Ngrok TCP Tunnel
+#Start ngrok TCP
 brew install --cask ngrok
 ngrok authtoken $1
 ngrok tcp 5900 --region=in &
